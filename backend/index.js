0dddf9771282b6adb5ae454f44fea28efc954f5f
hashedonemilliontimes <gavinmilligan1997@gmail.com>
2025-04-10 14:57:56 -0700

    update

--- backend/index.js
+++ backend/index.js
@@ -52,6 +52,7 @@ app.use(express.json());
 // Add middleware logging
 app.use((req, res, next) => {
     console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+    console.log('Headers:', req.headers);
     next();
   });
 
