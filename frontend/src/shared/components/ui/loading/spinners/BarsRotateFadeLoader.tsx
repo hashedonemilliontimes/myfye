8837332f9bdb4dbc13de635630df0a78cba9f530
Chadwick <hello@chadwickmacmillan.com>
2025-07-21 19:38:28 -0400

    feat(components): add bars rotate fade loader

---
+++ frontend/src/shared/components/ui/loading/spinners/BarsRotateFadeLoader.tsx
@@ -0,0 +1,120 @@
+import { css, keyframes } from "@emotion/react";
+import { Ref, SVGAttributes } from "react";
+
+interface BarsRotateFadeLoaderProps extends SVGAttributes<SVGSVGElement> {
+  ref?: Ref<SVGSVGElement>;
+  width?: number;
+  height?: number;
+  dur?: string;
+}
+
+const anim = keyframes`
+            8.3% {
+              transform: rotate(30deg);
+            }
+            16.6% {
+              transform: rotate(60deg);
+            }
+            25% {
+              transform: rotate(90deg);
+            }
+            33.3% {
+              transform: rotate(120deg);
+            }
+            41.6% {
+              transform: rotate(150deg);
+            }
+            50% {
+              transform: rotate(180deg);
+            }
+            58.3% {
+              transform: rotate(210deg);
+            }
+            66.6% {
+              transform: rotate(240deg);
+            }
+            75% {
+              transform: rotate(270deg);
+            }
+            83.3% {
+              transform: rotate(300deg);
+            }
+            91.6% {
+              transform: rotate(330deg);
+            }
+            100% {
+              transform: rotate(360deg);
+            }`;
+
+const BarsRotateFadeLoader = ({
+  ref,
+  height = 24,
+  width = 24,
+  dur = "0.75s",
+  ...restProps
+}: BarsRotateFadeLoaderProps) => {
+  return (
+    <svg
+      {...restProps}
+      ref={ref}
+      width={width}
+      height={height}
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g
+        className="spinner_OSmW"
+        css={css`
+          transform-origin: center;
+          animation: ${anim} ${dur} step-end infinite;
+          }
+        `}
+      >
+        <rect x="11" y="1" width="2" height="5" opacity=".14" />
+        <rect
+          x="11"
+          y="1"
+          width="2"
+          height="5"
+          transform="rotate(30 12 12)"
+          opacity=".29"
+        />
+        <rect
+          x="11"
+          y="1"
+          width="2"
+          height="5"
+          transform="rotate(60 12 12)"
+          opacity=".43"
+        />
+        <rect
+          x="11"
+          y="1"
+          width="2"
+          height="5"
+          transform="rotate(90 12 12)"
+          opacity=".57"
+        />
+        <rect
+          x="11"
+          y="1"
+          width="2"
+          height="5"
+          transform="rotate(120 12 12)"
+          opacity=".71"
+        />
+        <rect
+          x="11"
+          y="1"
+          width="2"
+          height="5"
+          transform="rotate(150 12 12)"
+          opacity=".86"
+        />
+        <rect x="11" y="1" width="2" height="5" transform="rotate(180 12 12)" />
+      </g>
+    </svg>
+  );
+};
+
+export default BarsRotateFadeLoader;
