8cef8537b1bb9d5285a92008401676030bf133e6
Sam Yeh <samheyyeh@gmail.com>
2025-08-05 14:09:48 -0500

    asset cards

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -36,6 +36,13 @@ export default defineConfig({
   },
   server: {
     port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
   build: {
     chunkSizeWarningLimit: 1500,
