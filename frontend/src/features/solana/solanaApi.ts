617306fe0dff4d5e29b0800b559defef4588bcba
Chadwick <hello@chadwickmacmillan.com>
2025-08-06 14:27:50 -0400

    feat: add solana api

---
+++ frontend/src/features/solana/solanaApi.ts
@@ -0,0 +1,49 @@
+import { MYFYE_BACKEND, MYFYE_BACKEND_KEY } from "@/env";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const solanaApi = createApi({
+  reducerPath: "solanaApi",
+  tagTypes: ["Solana"],
+  baseQuery: fetchBaseQuery({
+    baseUrl: MYFYE_BACKEND,
+    prepareHeaders: (headers) => {
+      headers.set("x-api-key", MYFYE_BACKEND_KEY);
+      return headers;
+    },
+    mode: "cors",
+    credentials: "include",
+  }),
+  endpoints: (build) => ({
+    saveRecentlyUsedAddresses: build.query<
+      unknown,
+      { userId: string; addresses: string[] }
+    >({
+      query: ({ userId, addresses }) => {
+        return {
+          url: `/save_recently_used_addresses`,
+          method: "POST",
+          body: {
+            user_id: userId,
+            addresses,
+          },
+        };
+      },
+    }),
+    getRecentlyUsedAddresses: build.query<unknown, string>({
+      query: (userId) => {
+        return {
+          url: `/get_recently_used_addresses`,
+          method: "POST",
+          body: {
+            user_id: userId,
+          },
+        };
+      },
+    }),
+  }),
+});
+
+export const {
+  useSaveRecentlyUsedAddressesQuery,
+  useGetRecentlyUsedAddressesQuery,
+} = solanaApi;
