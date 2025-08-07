51525123b86c920293554b96238ffa2e4a7d1a83
Chadwick <hello@chadwickmacmillan.com>
2025-08-07 17:39:22 -0400

    misc. solana updates

--- frontend/src/features/solana/solanaApi.ts
+++ frontend/src/features/solana/solanaApi.ts
@@ -1,6 +1,12 @@
 import { MYFYE_BACKEND, MYFYE_BACKEND_KEY } from "@/env";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+interface RecentlyUsedAddressesResponse {
+  id: string;
+  user_id: string;
+  addresses: string[];
+}
+
 export const solanaApi = createApi({
   reducerPath: "solanaApi",
   tagTypes: ["Solana"],
@@ -29,7 +35,10 @@ export const solanaApi = createApi({
         };
       },
     }),
-    getRecentlyUsedAddresses: build.query<unknown, string>({
+    getRecentlyUsedAddresses: build.query<
+      RecentlyUsedAddressesResponse,
+      string
+    >({
       query: (userId) => {
         return {
           url: `/get_recently_used_addresses`,
