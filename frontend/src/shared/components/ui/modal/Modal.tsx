2c5ba02a8f5e44e9743cbd2530642a0c4b437228
Chadwick <hello@chadwickmacmillan.com>
2025-08-08 14:39:38 -0400

    fix: use own component for modal management

--- frontend/src/shared/components/ui/modal/Modal.tsx
+++ frontend/src/shared/components/ui/modal/Modal.tsx
@@ -19,7 +19,9 @@ import {
   DialogPanel,
   DialogTitle,
 } from "@headlessui/react";
-import { ReactNode, Ref, useEffect, useState } from "react";
+import { ReactNode, Ref, RefObject, useEffect, useState } from "react";
+import Portal from "../portal/Portal";
+import { useModal } from "./useModal";
 
 // Wrap React Aria modal components so they support framer-motion values.
 const MotionDialogBackdrop = motion(DialogBackdrop);
@@ -46,6 +48,7 @@ export interface ModalProps extends HTMLMotionProps<"div"> {
   isOpen: boolean;
   children: ReactNode;
   onExit?: () => void;
+  initialFocus?: RefObject<HTMLElement>;
   ref?: Ref<HTMLDivElement>;
 }
 
@@ -60,6 +63,7 @@ const Modal = ({
   zIndex = 1000,
   onExit,
   ref,
+  initialFocus,
   children,
   ...restProps
 }: ModalProps) => {
@@ -89,145 +93,147 @@ const Modal = ({
       if (h.get() === height.get()) setInitialY(h.get());
     });
 
+  useModal({ isOpen, onOpenChange, ref, initialFocus });
+
   return (
     <>
-      <AnimatePresence onExitComplete={onExit}>
-        {isOpen && (
-          <MotionDialog
-            open
-            onClose={() => onOpenChange(false)}
-            css={css`
-              isolation: isolate;
-              position: fixed;
-              inset: 0;
-              z-index: ${zIndex};
-            `}
-          >
-            <MotionDialogBackdrop
-              style={{ backgroundColor: bg }}
+      <Portal containerId="modals">
+        <AnimatePresence onExitComplete={onExit}>
+          {isOpen && (
+            <motion.div
+              role="region"
               css={css`
-                position: absolute;
-                width: 100%;
-                height: 100%;
-              `}
-            />
-            <MotionDialogPanel
-              css={css`
-                display: grid;
-                font-family: var(--font-family);
-                grid-template-rows: auto 1fr;
-                position: absolute;
+                isolation: isolate;
+                position: fixed;
                 inset: 0;
-                top: auto;
-                margin-inline: auto;
-                max-width: var(--app-max-width);
-                width: 100%;
-                border-radius: var(--border-radius-medium);
-                border-bottom-left-radius: 0;
-                border-bottom-right-radius: 0;
-                box-shadow: var(--box-shadow-modal);
-                will-change: transform;
-                background-color: var(--clr-surface);
-                z-index: 1;
+                z-index: ${zIndex};
               `}
-              initial={{ y: initialY }}
-              animate={{ y: 0 }}
-              exit={{ y: initialY }}
-              transition={staticTransition}
-              style={{
-                y: y,
-                top: top,
-                // Extra padding at the bottom to account for rubber band scrolling.
-                paddingBottom: window.screen.height,
-              }}
-              drag="y"
-              dragConstraints={{ top: 0 }}
-              onDragStart={(e) => {
-                e.stopImmediatePropagation();
-              }}
-              onDrag={(e) => {
-                e.stopImmediatePropagation();
-              }}
-              onDragEnd={(e, { offset, velocity }) => {
-                e.stopImmediatePropagation();
-                if (offset.y > window.innerHeight * 0.75 || velocity.y > 10) {
-                  onOpenChange(false);
-                } else {
-                  animate(y, 0, { ...inertiaTransition, min: 0, max: 0 });
-                }
-              }}
-              {...restProps}
             >
               <motion.div
-                style={{
-                  height: h,
-                }}
+                style={{ backgroundColor: bg }}
+                css={css`
+                  position: absolute;
+                  width: 100%;
+                  height: 100%;
+                `}
+              />
+              <motion.div
                 css={css`
                   display: grid;
+                  font-family: var(--font-family);
                   grid-template-rows: auto 1fr;
-                  gap: var(--size-200);
+                  position: absolute;
+                  inset: 0;
+                  top: auto;
+                  margin-inline: auto;
+                  max-width: var(--app-max-width);
+                  width: 100%;
+                  border-radius: var(--border-radius-medium);
+                  border-bottom-left-radius: 0;
+                  border-bottom-right-radius: 0;
+                  box-shadow: var(--box-shadow-modal);
+                  will-change: transform;
+                  background-color: var(--clr-surface);
+                  z-index: 1;
                 `}
+                initial={{ y: initialY }}
+                animate={{ y: 0 }}
+                exit={{ y: initialY }}
+                transition={staticTransition}
+                style={{
+                  y: y,
+                  top: top,
+                  // Extra padding at the bottom to account for rubber band scrolling.
+                  paddingBottom: window.screen.height,
+                }}
+                drag="y"
+                dragConstraints={{ top: 0 }}
+                onDragStart={(e) => {
+                  e.stopImmediatePropagation();
+                }}
+                onDrag={(e) => {
+                  e.stopImmediatePropagation();
+                }}
+                onDragEnd={(e, { offset, velocity }) => {
+                  e.stopImmediatePropagation();
+                  if (offset.y > window.innerHeight * 0.75 || velocity.y > 10) {
+                    onOpenChange(false);
+                  } else {
+                    animate(y, 0, { ...inertiaTransition, min: 0, max: 0 });
+                  }
+                }}
+                {...restProps}
               >
-                <div
-                  data-drag-affordance
-                  css={css`
-                    margin-inline: auto;
-                    width: var(--size-800);
-                    height: var(--size-050);
-                    background-color: var(--clr-neutral-300);
-                    margin-block-start: var(--size-100);
-                    border-radius: var(--border-radius-pill);
-                  `}
-                />
-                <div
+                <motion.div
+                  style={{
+                    height: h,
+                  }}
                   css={css`
                     display: grid;
                     grid-template-rows: auto 1fr;
-                    gap: var(--size-300);
+                    gap: var(--size-200);
                   `}
                 >
-                  <header
+                  <div
+                    data-drag-affordance
                     css={css`
-                      position: relative;
-                      padding-inline: var(--size-200);
+                      margin-inline: auto;
+                      width: var(--size-800);
+                      height: var(--size-050);
+                      background-color: var(--clr-neutral-300);
+                      margin-block-start: var(--size-100);
+                      border-radius: var(--border-radius-pill);
+                    `}
+                  />
+                  <div
+                    css={css`
+                      display: grid;
+                      grid-template-rows: auto 1fr;
+                      gap: var(--size-300);
                     `}
                   >
-                    <DialogTitle
-                      as="h2"
-                      className="heading-small"
+                    <header
                       css={css`
-                        text-align: center;
+                        position: relative;
+                        padding-inline: var(--size-200);
                       `}
                     >
-                      {title}
-                    </DialogTitle>
-                    <Button
-                      onPress={() => onOpenChange(false)}
-                      iconOnly
-                      variant="transparent"
-                      icon={X}
+                      <h2
+                        className="heading-small"
+                        css={css`
+                          text-align: center;
+                        `}
+                      >
+                        {title}
+                      </h2>
+                      <Button
+                        onPress={() => onOpenChange(false)}
+                        iconOnly
+                        variant="transparent"
+                        icon={X}
+                        css={css`
+                          position: absolute;
+                          inset: 0;
+                          left: auto;
+                          right: var(--size-100);
+                          margin: auto;
+                        `}
+                      ></Button>
+                    </header>
+                    <main
                       css={css`
-                        position: absolute;
-                        inset: 0;
-                        left: auto;
-                        right: var(--size-100);
-                        margin: auto;
+                        container: modal-content / size;
                       `}
-                    ></Button>
-                  </header>
-                  <main
-                    css={css`
-                      container: modal-content / size;
-                    `}
-                  >
-                    {children}
-                  </main>
-                </div>
+                    >
+                      {children}
+                    </main>
+                  </div>
+                </motion.div>
               </motion.div>
-            </MotionDialogPanel>
-          </MotionDialog>
-        )}
-      </AnimatePresence>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </Portal>
     </>
   );
 };
